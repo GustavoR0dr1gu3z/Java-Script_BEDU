afb8af56a4a209c8a0521f319ccaf09110c37564
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-21 20:30:59 -0500

    Cambiando hora

--- ejemplos-sesion-2.js
+++ ejemplos-sesion-2.js
@@ -2,15 +2,19 @@ const time = 13;
 let greeting;
 
 if (time < 12) {
-  greeting = "Good morning";
+    greeting = "Good morning";
 }
 
 if (time < 20) {
-  greeting = "Good afternoon";
+    greeting = "Good afternoon";
 }
 
 if (time >= 20) {
-  greeting = "Good evening";
+    greeting = "Good evening";
 }
 
-console.log(greeting) // Good afternoon
+if (time < 1 && time > 23){
+    greeting = "No existe esa hora"
+}
+
+console.log(greeting)
