ca100a16b974a55bc81302aece0e0030eb369a7c
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-21 21:01:46 -0500

    Reto del diametro y grosor de una rueda

---
+++ reto.js
@@ -0,0 +1,16 @@
+const diametro = 1.5;
+const grosor = 0.2;
+let mensaje;
+
+if (diametro>1.4){
+    mensaje = "La rueda es para un vehiculo grande";
+}else if(diametro <= 1.4 && diametro > 0.8){
+    mensaje = "La rueda es para un vehiculo mediano";
+}else if(diametro>1.4 && grosor<0.4 || diametro <= 1.4 && diametro > 0.8 && grosor > 0.25){
+    mensaje ="El grosor para esta rueda es inferior al recomendado";
+}else{
+    mensaje="La rueda es para un vehiculo pequeño";    
+}
+
+
+console.log(mensaje);
