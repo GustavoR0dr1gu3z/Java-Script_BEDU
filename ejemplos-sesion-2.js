83881b9d311646f62ae96aa8cb100ff8e24206ea
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-21 20:43:07 -0500

    Terminando condiciones con tiempo

--- ejemplos-sesion-2.js
+++ ejemplos-sesion-2.js
@@ -1,17 +1,21 @@
-const time = 13;
+const time = 25;
 let greeting;
 
 if (time > 0 && time < 12) {
     greeting = "Good morning";
-    
-}else if (time <= 12 && time < 20) {
+}
+
+else if (time >= 12 && time < 20) {
     greeting = "Good afternoon";
+}
 
-}else if (time >= 20) {
+else if (time >= 20 && time < 23) {
     greeting = "Good evening";
+}
 
-}else(time < 23){
+else{
     greeting = "No existe esa hora";
 }
 
 console.log(greeting)
+
