6def26308985908d64ea73b2320fb723321004c2
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-26 20:05:48 -0500

    imprimiendo numeros

--- s3/s3.js
+++ s3/s3.js
@@ -2,5 +2,8 @@ const numbers = [1,2,3,4,5,6,7,8,9,0]
 const doubled _= []
 
 for(let i=0; i<numbers.length; i++){
-    doubled.push(i)
+    doubled.push(numbers[i]*2)
 }
+
+console.log(numbers)
+console.log(doubled)
