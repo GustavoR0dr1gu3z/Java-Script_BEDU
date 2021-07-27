b71973759dc03a39a559c1a85f18f03617f29403
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-26 20:20:25 -0500

    otra forma para imprimir, con const

--- s3/s3.js
+++ s3/s3.js
@@ -1,8 +1,13 @@
 const numbers = [1,2,3,4,5,6,7,8,9,0]
 const doubled = []
-
+/*
 for(let i=0; i<numbers.length; i++){
     doubled.push(numbers[i]*2)
+}
+*/
+
+for(const number of numbers){
+    doubled.push(number*2)
 }
 
 console.log(numbers)
