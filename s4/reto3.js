10b37a7eeaccb6478adca83be4ee42ea5e2494da
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-28 21:59:33 -0500

    serie de fibonacci

--- s4/reto3.js
+++ s4/reto3.js
@@ -1,8 +1,23 @@
-function fibonacciSequence(limit){
+fibonacci = [1]
 
+function fibonacciSequence(limit){
+    var a = 0;
+    var b = 1;
+
+    for(let i = 0; i<limit-1; i++){
+        if(limit == 1){
+            break;
+        }else{
+            c = a+b;
+            a = b;
+            b = c;
+            fibonacci.push(b);
+        }
+    }
+    console.log(fibonacci)
 }
 
-
 fibonacciSequence(1);
-fibonacciSequence(2);
 fibonacciSequence(5);
+
+
