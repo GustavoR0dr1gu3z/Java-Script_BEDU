54ca37dbfae613e517f3e911b12870d2cfd2e4a5
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-28 20:06:41 -0500

    imprimir funcion para calcular año

---
+++ s4/ejemplo1.js
@@ -0,0 +1,12 @@
+function calcularAge(birthYear){
+    const age = 2021-birthYear
+    return (age)
+}
+
+const gusAge=calcularAge(1999);
+const otroGusAge=calcularAge(1859);
+const nuevoOtroGusAGe=calcularAge(1849);
+
+console.log(gusAge)
+console.log(otroGusAge)
+console.log(nuevoOtroGusAGe)
--- s4/sesion4-1.js
+++
@@ -1,8 +0,0 @@
-function calcularAge(birthYear){
-    const age = 2021-birthYear
-    return (age)
-}
-
-const gusAge=calcularAge(1999);
-
-console.log(gusAge)
