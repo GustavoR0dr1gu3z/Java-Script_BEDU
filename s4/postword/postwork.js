2c95cfa8465a5e1ee91f7c710cecd0462f747fda
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-30 18:14:48 -0500

    array bidimensional

--- s4/postword/postwork.js
+++ s4/postword/postwork.js
@@ -28,3 +28,24 @@ const john = {
    console.log('Test 3:', deepEqual(john, john)) // true
    console.log('Test 4:', deepEqual(john, { firstName: 'John', lastName: 'Doe' })) // true
    console.log('Test 5:', deepEqual(john, { firstName: 'John' })) // false
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+
+function chunk(array, size){
+    var data2 = []
+
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j<size; j++){
+            data2.push(array[j][i]);
+        }
+    }
+    return data2;
+}
+
+const data = [1, 2, 3, 4, 5, 6, 7, 8]
+
+console.log('Test 1:', chunk(data, 1)) // [[1], [2], [3], [4], [5], [6], [7], [8]]
+console.log('Test 2:', chunk(data, 2)) // [[1, 2], [3, 4], [5, 6], [7, 8]]
+console.log('Test 3:', chunk(data, 3)) // [[1, 2, 3], [4, 5, 6], [7, 8]]
