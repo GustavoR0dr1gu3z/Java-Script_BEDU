b068e955ac5c283d72da0f224f2527414d37174c
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-31 11:51:31 -0500

    manipulacion de array

--- s5/retoFinal.js
+++ s5/retoFinal.js
@@ -1,13 +1,10 @@
 const array1 = ['Hello'];
 
-
 const mutable = array => {
     const array1 = [...array];
     array1.push('World');
     return array1;
 }
-
-
 
 console.log(`Mi array1 al inicio: ${array1}`);
 console.log(`Mi array resultante: ${mutable(array1)}`);
