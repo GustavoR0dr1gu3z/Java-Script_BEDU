15e06476b19fd4a6e4fa65e3809cf4b6c2275a1c
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-30 19:43:47 -0500

    scope local

--- s5/ejemplo1.js
+++ s5/ejemplo1.js
@@ -1,3 +1,4 @@
+// SCOPE GLOBAL
 const name = "John Doe";
 
 console.log(name); // "John Doe"
@@ -7,3 +8,15 @@ function foo() {
 }
 
 foo(); // "John Doe"
+
+
+//SCOPE LOCAL
+function foo() {
+    const bar = "John Doe";
+    console.log(bar); 
+  }
+  
+  foo(); // "John Doe"
+  
+  console.log(bar); // Uncaught ReferenceError: name is not defined
+  
