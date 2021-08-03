436543ac569d913cf96cec8349724dbdaca9ab43
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-02 21:56:58 -0500

    prototype con el constructor

---
+++ s6/reto3.js
@@ -0,0 +1,16 @@
+const Triangle = function(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    return ("a: "+this.a+", b: "+this.b+", c:"+this.c)
+}
+
+Triangle.prototype.getPerimeter = function(){
+    return (this.a+this.b+this.c);
+}
+
+const triangle = new Triangle(1, 2, 3);
+
+  console.log(triangle); // Triangle { a: 1, b: 2, c: 3 }
+  console.log(triangle.getPerimeter()); // 6
