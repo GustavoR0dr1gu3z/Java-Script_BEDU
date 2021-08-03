661486bc075d4dd158470ee27b13af4c5afae3fc
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-02 21:16:21 -0500

    reto 2

---
+++ s6/reto2.js
@@ -0,0 +1,11 @@
+const Group = function() {
+    // Code goes here...
+}
+
+const group = Group.from([1, 2, 3, 4, 5]);
+console.log(group); // Group { members: [ 1, 2, 3, 4, 5 ] }
+console.log(group.has(5)); // true
+console.log(group.has(10)); // false
+
+group.add(10);
+console.log(group.has(10)); // true
