fbbc068518ea2a4d0262702c4dd707cda69be72b
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-02 19:54:26 -0500

    creando constructor person

---
+++ s6/ejemplo1.js
@@ -0,0 +1,12 @@
+const john = {
+    name: 'John',
+    birthYear: 1990,
+    job: 'Developer'
+  }
+  
+
+const Persona = function (name, birthYear, job){
+    this.name = namme,
+    this.birthYear = birthYear;
+    this.job = job
+}
