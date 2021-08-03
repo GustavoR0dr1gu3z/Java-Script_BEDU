a83945f5cf75cf77ed70526c28a58f34d2860184
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-02 20:15:04 -0500

    arreglos de constructores

--- s6/ejemplo1.js
+++ s6/ejemplo1.js
@@ -13,4 +13,13 @@ const Persona = function (name, birthYear, job){
 
 const johnPersona = new Persona('Gustavo', 1999, 'Web Developer');
 
+const Persons = [
+    new Person('Gus', 1998, 'Web Movil'),
+    new Person('Gustav', 1997, 'AI'),
+    new Person('Tavo', 1996, 'Redes'),
+    new Person('Gusi', 1995, 'Electronica')
+]
+
+
+
 console.log(johnPersona);
