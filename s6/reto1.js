6f6aa0c8ae971375e12f964112770e1ca031b8f8
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-02 20:16:17 -0500

    Reto 1

---
+++ s6/reto1.js
@@ -0,0 +1,10 @@
+const Vec = function(x, y) {
+    // Code goes here...
+}
+
+const vec1 = new Vec(1, 2);
+const vec2 = new Vec(2, 3);
+
+  console.log(vec1.plus(vec2)); // Vec { x: 3, y: 5 }
+  console.log(vec1.minus(vec2)); // Vec { x: -1, y: -1 }
+  console.log(vec1.length()); // 2.23606797749979
