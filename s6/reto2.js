d5bacb23f098bfdd5be40136390e4cc04781a21c
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-02 21:42:54 -0500

    herencia con funciones y costructor

--- s6/reto2.js
+++ s6/reto2.js
@@ -1,6 +1,23 @@
-const Group = function() {
-    // Code goes here...
+const Group = function(arreglo) {
+    this.array = arreglo;
+
+    this.add = function(valor1) {
+        if (this.array.includes(valor1) == false){
+            this.array.push(valor1)
+        }
+    };
+
+    this.has = function(valor1){
+        return(this.array.includes(valor1));
+    }
+
 }
+
+Group.from = function(arreglo){
+    const group = new Group(arreglo);
+    return group;
+};
+
 
 const group = Group.from([1, 2, 3, 4, 5]);
 console.log(group); // Group { members: [ 1, 2, 3, 4, 5 ] }
