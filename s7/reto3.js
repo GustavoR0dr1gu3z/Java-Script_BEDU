2360cd89659eab1fe5849d5666badd5bd98b2cd4
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-04 18:49:42 -0500

    reto 3

---
+++ s7/reto3.js
@@ -0,0 +1,16 @@
+function loop(start, test, update, body) {
+    // Code goes here...
+}
+
+const test = function(n) {
+    return n > 0;
+}
+
+const update = function(n) {
+    return n - 1;
+}
+
+loop(3, test, update, console.log);
+  // 3
+  // 2
+  // 1
