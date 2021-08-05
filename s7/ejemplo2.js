3503983aae3e77e9294c67f3ec2a8ee0c1807092
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-04 21:17:30 -0500

    split

--- s7/ejemplo2.js
+++ s7/ejemplo2.js
@@ -8,4 +8,9 @@ nsToStr.split('')
 
 
 // mapeando y conviertiendo a entero cada elemento
-nsToStr.split('').map( n => Number(n) )
+//nsToStr.split('').map( n => Number(n) )
+
+// Sumando cada elemento
+nsToStr.split('').map(Number).reduce((acc, e)=>acc+e, 0)
+
+console.log(nsToStr);
