b803e85806bf9828aec40d3a451e2f288c7ebe23
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-06 20:19:10 -0500

    inicalizar npm

--- s8-1/webpack.config.js
+++ s8-1/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const = HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
 // LEER EL HTML
+module.exports = {
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
