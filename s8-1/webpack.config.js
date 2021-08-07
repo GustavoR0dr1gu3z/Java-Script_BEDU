654714d955ebdc069914bc75f17018ef5a49a22a
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-06 20:25:47 -0500

    package json mandar a llamar webpack

--- s8-1/webpack.config.js
+++ s8-1/webpack.config.js
@@ -1,18 +1,16 @@
 const path = require('path');
-const = HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// LEER EL HTML
 module.exports = {
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-
   plugins: [
-    new HtmlWebpackPlugin{
-        filename: 'index.html',
-        template: './src/index.html'
-    }
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
   ]
 }
