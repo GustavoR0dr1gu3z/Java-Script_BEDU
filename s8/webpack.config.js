77ee508a5dd80bf278556dbcbd8b680166f48684
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-06 21:44:57 -0500

    le digo a webpack que entienda babel

--- s8/webpack.config.js
+++ s8/webpack.config.js
@@ -16,6 +16,16 @@ module.exports = {
   module: {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
     ]
   },
   devServer: {
