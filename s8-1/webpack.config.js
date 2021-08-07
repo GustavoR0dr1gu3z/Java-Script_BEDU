06cbb3edbffc2e70ef6e7848a89624521b35d4d4
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-06 20:15:33 -0500

    Webpack

--- s8-1/webpack.config.js
+++ s8-1/webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const = HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+// LEER EL HTML
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin{
+        filename: 'index.html',
+        template: './src/index.html'
+    }
+  ]
 }
