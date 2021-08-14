053562f42c801af13506ee0557f8b82af3636b87
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-13 20:14:14 -0500

    asincronismo

--- s11/index.js
+++ s11/index.js
@@ -1,7 +1,10 @@
 function startProcess() {
     console.log('Start Phase 1. Wait one second...');
     setTimeout(function () {
-        console.log('Phase 1 completed.');
+        console.log('Phase 1 completed. Wait two seconds...');
+        setTimeout(function () {
+            console.log('Phase 2 completed.');
+        }, 2000);
     }, 1000);
 }
 
