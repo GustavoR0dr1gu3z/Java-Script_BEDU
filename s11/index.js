a07efdccd2f3f427eab50a8f0b3d7548edcff4e9
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-08-13 20:08:34 -0500

    funcion de esperar un segundo

---
+++ s11/index.js
@@ -0,0 +1,8 @@
+function startProcess() {
+    console.log('Start Phase 1. Wait one second...');
+    setTimeout(function () {
+        console.log('Phase 1 completed.');
+    }, 1000);
+}
+
+startProcess();
